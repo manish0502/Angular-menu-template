aca30b2b6999b3977f011e967466b917421d70c2
Manish kumar giri <manish.kumar@ults.in>
2020-12-24 12:44:26 +0530

    Angular-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
   title = 'temmp';
+
+  ngOnInit() {
+    //Toggle Click Function
+$("#menu-toggle").click(function(e) {
+e.preventDefault();
+$("#wrapper").toggleClass("toggled");
+});
+}
+
+
 }
